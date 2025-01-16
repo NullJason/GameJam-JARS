576905ced97a7e10283b4696a828c5a90b2b4f2c
NullJason <159076558+NullJason@users.noreply.github.com>
2025-01-16 14:15:23 -0500

    moved the proj repo somewhere else

--- DungeonCrawler/Assets/ConnectToMap.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class ConnectToMap : MonoBehaviour
-{
-   private void OnTriggerEnter2D(Collider2D other) {
-      if(other != null && MapManager.Instance != null) MapManager.Instance.OnPlrCollided(other);
-   }
-}
