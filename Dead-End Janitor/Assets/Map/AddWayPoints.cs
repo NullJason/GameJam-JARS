95269bd3edaaa3cf47e4fd26279abd10b458f2d6
Stephen <159204715+StephenK0@users.noreply.github.com>
2025-01-21 14:24:25 -0500

    MC now changes speed depending on action. Waypoints may now be marked to cause the MC to wait for several seconds.

---
+++ Dead-End Janitor/Assets/Map/AddWayPoints.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+//This script is meant to be used to add waypoints more easily. Attach to the player. Press 1 to add a new path. Press 2 to add a new waypoint to a path.
+//TODO!
+
+public class AddWayPoints : MonoBehaviour
+{
+/*  public GameObject waypoint;
+  public Waypoint current;
+  public GameObject path;
+  public GameObject currentPath;
+  // Update is called once per frame
+  void Update()
+  {
+    if(Input.GetKeyDown('1')){
+      Waypoint newPoint = Instantiate(path, transform.position, transfrom.rotation, currentPath).GetComponent<Waypoint>();
+      current.SetNext(newPoint);
+      current = newPoint;
+    }
+//    else if(Input.GetKeyDown('2'))
+  }*/
+}
