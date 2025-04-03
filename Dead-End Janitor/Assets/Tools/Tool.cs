0c3c5916b9f790f1e7443937d5b7a5d145e560dc
Stephen <159204715+StephenK0@users.noreply.github.com>
2025-04-03 15:37:38 -0400

    Update Tool enum to accomodate upgraded tools.

--- Dead-End Janitor/Assets/Tools/Tool.cs
+++ Dead-End Janitor/Assets/Tools/Tool.cs
@@ -1,8 +1,12 @@
 public enum Tool{
   mop = 1,
-  vacuum = 2,
-  spray = 3,
-  sponge = 4,
-  featherDuster = 5,
-  slippySoap = 6
+  mopII = 2,
+  mopIII = 3,
+  vacuum = 4,
+  vacuumII = 5,
+  vacuumIII = 6,
+  spray = 7,
+  sponge = 8,
+  featherDuster = 9,
+  slippySoap = 10
 }
