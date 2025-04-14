7c043fade7682481c05b991b2b6c7ff3851957f9
Stephen <159204715+StephenK0@users.noreply.github.com>
2025-04-14 17:07:20 -0400

    Update functionality to BackButton.cs to support closing multiple gameobjects. Add special tab to shop.

--- Dead-End Janitor/Assets/Player/UI/Back Button.cs
+++ Dead-End Janitor/Assets/Player/UI/Back Button.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-//A button that disables one gameObject and opens another. Good for a button that closes one section of the menu and opens another.
+//A button that disables one or more gameObjects and opens exactly one other. Good for a button that closes one section of the menu and opens another.
 
 public class BackButton : ButtonFunctionality
 {
-  [SerializeField] GameObject close;
+  [SerializeField] GameObject[] close;
   [SerializeField] GameObject open;
   private protected override void Start(){
     base.Start();
-    if(close == null) close = gameObject;
+    if(close == null) {
+      close = new GameObject[1];
+      close[0] = gameObject;
+    }
     if(close == null) Debug.Log("No object specified!");
   }
   private protected override void ButtonDo(){
-    close.SetActive(false);
+    foreach(GameObject g in close) g.SetActive(false);
     open.SetActive(true);
   }
 }
